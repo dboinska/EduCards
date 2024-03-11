b42a8788149335fc71888da7b6dbc633f50ec5bb
Dorota Boinska <dboinska@gmail.com>
2024-03-12 00:26:44 +0100

    feat(components): add SegmentedControl to BottomBar

--- src/components/BottomBar.tsx
+++ src/components/BottomBar.tsx
@@ -1,33 +1,66 @@
-import { IconMessage, IconHeart } from "@tabler/icons-react"
 import Icon from "@mdi/react"
-import { mdiFolderOutline, mdiFolderAccountOutline } from "@mdi/js"
 import classes from "../styles/BottomBar.module.css"
-import { Flex } from "@mantine/core"
-import Link from "next/link"
 import { useNavigationLinks } from "@/hooks/useNavigationLinks"
+import { SegmentedControl } from "@mantine/core"
+import { useRouter } from "next/router"
 
+type SegmentData = {
+  label: JSX.Element
+  value: string
+}
+//
 export function BottomBar() {
   const { menuLinks, isCurrentPath, currentUser } = useNavigationLinks()
 
-  const menu = menuLinks.map((link) => (
-    <Link
-      key={link.alias}
-      className={classes.link}
-      data-active={isCurrentPath(link)}
-      href={link.path}
-    >
-      <Flex align="center">
-        <Icon path={link.icon!} size={1} />
-        <span className={classes.linkAlias}>{link.alias}</span>
-      </Flex>
-    </Link>
-  ))
+  const router = useRouter()
+
+  const segmentsData = menuLinks.map((link) => {
+    const isActive = router.asPath === link.path.href
+    const isAnySegmentActive = menuLinks.some((link) => router.asPath === link.path.href)
+
+    return {
+      label: (
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <Icon
+            className="icon"
+            style={{
+              color: isActive ? "white" : "var(--mantine-color-lime-filled)",
+            }}
+            path={link.icon}
+            size={1}
+          />
+          <span
+            style={{
+              marginLeft: "4px",
+              display: isActive && isAnySegmentActive ? "block" : "none",
+              width: !isActive ? "0" : "auto",
+            }}
+          >
+            {link.alias}
+          </span>
+        </div>
+      ),
+      value: link.path.href,
+    }
+  })
+
+  const handleSegmentChange = async (value: string) => {
+    try {
+      await router.push(value)
+    } catch (error) {
+      console.error("Error during navigation:", error)
+    }
+  }
 
   return (
-    <Flex align="center" hiddenFrom="md" className={classes.container}>
-      <Flex justify="space-between" className={classes.bottomBar}>
-        {menu}
-      </Flex>
-    </Flex>
+    <SegmentedControl
+      hiddenFrom="md"
+      radius="xl"
+      size="md"
+      data={segmentsData}
+      value={router.asPath}
+      onChange={handleSegmentChange}
+      classNames={classes}
+    />
   )
 }
