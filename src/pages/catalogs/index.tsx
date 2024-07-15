8371324d95a0175f37b7c7bc3bbfe6059133987b
Dorota Boinska <dboinska@gmail.com>
2024-07-16 01:32:43 +0200

    refactor(pages): optimize imports and restructure sort order definition

--- src/pages/catalogs/index.tsx
+++ src/pages/catalogs/index.tsx
@@ -1,18 +1,17 @@
 import Layout from "src/core/layouts/Layout"
 import { BlitzPage, Routes } from "@blitzjs/next"
 import styles from "src/styles/Catalogs.module.css"
-import Link from "next/link"
-import { Avatar, Box, Flex, Badge } from "@mantine/core"
+import { Box, Flex } from "@mantine/core"
 import { useCurrentUser } from "@/users/hooks/useCurrentUser"
 import { useReducer, useState } from "react"
 import { CatalogHeader } from "@/components/CatalogHeader"
 import { Switch } from "@/components/Switch"
-import { Picker, PickerOption } from "@/components/Picker"
-import { ToggleMenu } from "@/components/ToggleMenu"
+import { Picker } from "@/components/Picker"
 import { TextInput } from "@mantine/core"
 import { useDebouncedCallback } from "@mantine/hooks"
 
 import type { PayloadMap } from "@/types/PayloadMap"
+import type { PickerOption } from "@/components/Picker"
 
 import { useQuery } from "@blitzjs/rpc"
 import getCatalogs from "./queries/getCatalogs"
@@ -27,37 +26,6 @@ export interface CatalogProps {
   authorId: string
   numberOfCards?: number
 }
-
-const sharedWith = [
-  {
-    label: "Date ascending",
-    name: "Date ascending",
-    image:
-      "https://extraextrabricks.pl/environment/cache/images/500_500_productGfx_3935/Mini-figurka-LEGO-City-kobieta--mama-w-fioletowej-bluzie%2C-piaskowych-spodniach.webp",
-    value: "asc",
-  },
-  {
-    label: "Date descending",
-    name: "Date descending",
-    image:
-      "https://extraextrabricks.pl/environment/cache/images/500_500_productGfx_3935/Mini-figurka-LEGO-City-kobieta--mama-w-fioletowej-bluzie%2C-piaskowych-spodniach.webp",
-    value: "desc",
-  },
-  {
-    label: "Alphabetically",
-    name: "Alphabetically",
-    image:
-      "https://extraextrabricks.pl/environment/cache/images/500_500_productGfx_3935/Mini-figurka-LEGO-City-kobieta--mama-w-fioletowej-bluzie%2C-piaskowych-spodniach.webp",
-    value: "alfa_asc",
-  },
-  {
-    label: "Reverse alphabetically",
-    name: "Reverse alphabetically",
-    image:
-      "https://extraextrabricks.pl/environment/cache/images/500_500_productGfx_3935/Mini-figurka-LEGO-City-kobieta--mama-w-fioletowej-bluzie%2C-piaskowych-spodniach.webp",
-    value: "alfa_desc",
-  },
-] as const
 
 const catalogSettings = [
   {
@@ -137,20 +105,47 @@ const initialState = {
   filter: "all",
 }
 
+interface SortOrder extends Omit<PickerOption, "value"> {
+  value: SortType
+}
+
+const sortOrder: SortOrder[] = [
+  {
+    label: "Date ascending",
+    imageAlt: "Date ascending",
+    image:
+      "https://extraextrabricks.pl/environment/cache/images/500_500_productGfx_3935/Mini-figurka-LEGO-City-kobieta--mama-w-fioletowej-bluzie%2C-piaskowych-spodniach.webp",
+    value: sortTypes.ASC,
+  },
+  {
+    label: "Date descending",
+    imageAlt: "Date descending",
+    image:
+      "https://extraextrabricks.pl/environment/cache/images/500_500_productGfx_3935/Mini-figurka-LEGO-City-kobieta--mama-w-fioletowej-bluzie%2C-piaskowych-spodniach.webp",
+    value: sortTypes.DESC,
+  },
+  {
+    label: "Alphabetically",
+    imageAlt: "Alphabetically",
+    image:
+      "https://extraextrabricks.pl/environment/cache/images/500_500_productGfx_3935/Mini-figurka-LEGO-City-kobieta--mama-w-fioletowej-bluzie%2C-piaskowych-spodniach.webp",
+    value: sortTypes.ALFA_ASC,
+  },
+  {
+    label: "Reverse alphabetically",
+    imageAlt: "Reverse alphabetically",
+    image:
+      "https://extraextrabricks.pl/environment/cache/images/500_500_productGfx_3935/Mini-figurka-LEGO-City-kobieta--mama-w-fioletowej-bluzie%2C-piaskowych-spodniach.webp",
+    value: sortTypes.ALFA_DESC,
+  },
+] as const
+
 const Catalogs: BlitzPage = ({ id, image, header, desc, isFavorite, authorId }: CatalogProps) => {
   const [catalogState, dispatch] = useReducer(dataReducer, { ...initialState })
-
   const [searchValue, setSearchValue] = useState("")
-
   const [catalog] = useQuery(getCatalogs, catalogState)
 
-  console.log({ catalogState })
-
-  // console.log({ catalog })
-
   const currentUser = useCurrentUser()
-
-  const [catalogType, setCatalogType] = useState<string>("all")
 
   const handleSearch = useDebouncedCallback(async (query: string) => {
     dispatch({
@@ -165,309 +160,6 @@ const Catalogs: BlitzPage = ({ id, image, header, desc, isFavorite, authorId }: 
     setSearchValue(event.currentTarget.value)
     handleSearch(event.currentTarget.value)
   }
-
-  const itemContent = (catalogItem) => (
-    <div
-      key={catalogItem.id}
-      className={`${catalogItem.imageUrl && styles.withOverlay} ${styles.body}`}
-      style={{
-        backgroundImage: `url(${catalogItem.url})`,
-      }}
-    >
-      <div className={catalogItem.imageUrl && styles.overlay}></div>
-      <Link className={styles.cardContent} href={Routes.Catalog()}>
-        <div className={styles.headerContainer}>
-          <h2>{catalogItem.name}</h2>
-
-          <Badge size="sm" variant="outline" color="var(--mantine-color-gray-3)">
-            {catalogItem.numberOfCards} cards
-          </Badge>
-        </div>
-        <h3>{catalogItem.description}</h3>
-      </Link>
-      <div className={styles.inline}>
-        <div className={styles.author}>
-          <Avatar
-            src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-2.png"
-            alt="Jacob Warnhalter"
-            radius="xl"
-            size="sm"
-          />
-          <span>Author</span>
-        </div>
-        <Flex className={styles.controls}>
-          {currentUser?.id === authorId && (
-            <ToggleMenu item={"catalog"} settings={catalogSettings} />
-          )}
-
-          {/* {favCard(catalogItem.isFavorite)} */}
-        </Flex>
-      </div>
-    </div>
-  )
-
-  // const publicCatalogs = catalog.map(
-  //   (catalogItem) => catalogItem.type === "public" && itemContent(catalogItem)
-  // )
-
-  // const sharedCatalogs = catalog.map(
-  //   (catalogItem) => catalogItem.type === "shared" && itemContent(catalogItem)
-  // )
-
-  // const ownCatalogs = catalog.map(
-  //   (catalogItem) => catalogItem.type === "own" && itemContent(catalogItem)
-  // )
-
-  // console.log({ catalogType })
-
-  // const catalogContent = (catalogType) => {
-  //   switch (catalogType) {
-  //     case "all":
-  //       return (
-  //         <>
-  //           {publicCatalogs}
-  //           {sharedCatalogs}
-  //           <div
-  //             className={`${styles.withOverlay} ${styles.body}`}
-  //             style={{
-  //               backgroundImage:
-  //                 "url(https://images.unsplash.com/photo-1530122037265-a5f1f91d3b99?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80)",
-  //             }}
-  //           >
-  //             <div className={styles.overlay}></div>
-  //             <Link className={styles.cardContent} href={Routes.Catalog()}>
-  //               <div className={styles.headerContainer}>
-  //                 <h2>NameNameNameNameNameNameNameName</h2>
-  //                 <Badge size="sm" variant="outline" color="var(--mantine-color-gray-3)">
-  //                   10 cards
-  //                 </Badge>
-  //               </div>
-  //               <h3>Description</h3>
-  //             </Link>
-  //             <div className={styles.inline}>
-  //               <div className={styles.author}>
-  //                 <Avatar
-  //                   src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-2.png"
-  //                   alt="Jacob Warnhalter"
-  //                   radius="xl"
-  //                   size="sm"
-  //                 />
-  //                 <span>Author</span>
-  //               </div>
-  //               <Flex className={styles.controls}>
-  //                 {currentUser?.id === authorId && (
-  //                   <ToggleMenu item={"catalog"} settings={catalogSettings} />
-  //                 )}
-
-  //                 {favCard(true)}
-  //               </Flex>
-  //             </div>
-  //           </div>
-  //           <div
-  //             className={`${styles.withOverlay} ${styles.body}`}
-  //             style={{
-  //               backgroundImage:
-  //                 "url(https://www.instalki.pl/wp-content/uploads/2021/02/atoms.jpg)",
-  //             }}
-  //           >
-  //             <div className={styles.overlay}></div>
-  //             <Link className={styles.cardContent} href={Routes.Catalog()}>
-  //               <div className={styles.headerContainer}>
-  //                 <h2>Lorem ipsum</h2>
-  //                 <span>10</span>
-  //               </div>
-  //               <h3>
-  //                 In eleifend velit eu neque mollis, rutrum malesuada leo luctus. Curabitur non
-  //                 mauris facilisis, fringilla ligula ac. In eleifend velit eu neque mollis, rutrum
-  //                 malesuada leo luctus. Curabitur non mauris facilisis, fringilla ligula ac.
-  //               </h3>
-  //             </Link>
-  //             <div className={styles.inline}>
-  //               <div className={styles.author}>
-  //                 {" "}
-  //                 <Avatar
-  //                   src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-2.png"
-  //                   alt="Jacob Warnhalter"
-  //                   radius="xl"
-  //                   size="sm"
-  //                 />
-  //                 <span>Author</span>
-  //               </div>
-
-  //               <div className={styles.controls}>
-  //                 <Link href={Routes.NewCatalog()}>
-  //                   <IconSettings size="22" style={{ color: "var(--mantine-color-gray-3)" }} />
-  //                 </Link>
-  //                 {favCard(false)}
-  //               </div>
-  //             </div>
-  //           </div>
-  //           <div
-  //             className={`${styles.withOverlay} ${styles.body}`}
-  //             style={{
-  //               backgroundImage:
-  //                 "url(https://www.podrb.pl/upload/user_content/warzy/winter-3088042-1920-1.jpg)",
-  //             }}
-  //           >
-  //             <div className={styles.overlay}></div>
-  //             <Link className={styles.cardContent} href={Routes.Catalog()}>
-  //               {" "}
-  //               <div className={styles.headerContainer}>
-  //                 <h2>Lorem ipsum</h2>
-  //                 <span>10</span>
-  //               </div>
-  //               <h3>
-  //                 In eleifend velit eu neque mollis, rutrum malesuada leo luctus. Curabitur non
-  //                 mauris facilisis, fringilla ligula ac.
-  //               </h3>
-  //             </Link>
-  //             <div className={styles.inline}>
-  //               <Avatar
-  //                 src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-2.png"
-  //                 alt="Jacob Warnhalter"
-  //                 radius="xl"
-  //                 size="sm"
-  //               />
-  //               <span>Author</span>
-  //             </div>
-  //           </div>
-  //           <div
-  //             className={`${styles.withOverlay} ${styles.body}`}
-  //             style={{
-  //               backgroundImage:
-  //                 "url(https://dc.sklep.pl/wp-content/uploads/2021/07/kropki-25x25cm.jpg)",
-  //             }}
-  //           >
-  //             <div className={styles.overlay}></div>
-  //             <Link className={styles.cardContent} href={Routes.Catalog()}>
-  //               <div className={styles.headerContainer}>
-  //                 <h2>Lorem ipsum</h2>
-  //                 <span>10</span>
-  //               </div>
-  //               <h3>In eleifend velit eu neque mollis, rutrum malesuada leo luctus.</h3>
-  //             </Link>
-  //             <div className={styles.inline}>
-  //               <Avatar
-  //                 src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-2.png"
-  //                 alt="Jacob Warnhalter"
-  //                 radius="xl"
-  //                 size="sm"
-  //               />
-  //               <span>Author</span>
-  //             </div>
-  //           </div>
-  //           <div
-  //             className={`${styles.withOverlay} ${styles.body}`}
-  //             style={{
-  //               backgroundImage: "url(https://planetescape.pl//app/uploads/2020/11/Flamingi.jpg)",
-  //             }}
-  //           >
-  //             <div className={styles.overlay}></div>
-  //             <Link className={styles.cardContent} href={Routes.Catalog()}>
-  //               <div className={styles.headerContainer}>
-  //                 <h2>Lorem ipsum</h2>
-  //                 <span>10</span>
-  //               </div>
-  //               <h3>In eleifend velit eu neque mollis, rutrum malesuada leo luctus.</h3>
-  //             </Link>
-  //             <div className={styles.inline}>
-  //               <Avatar
-  //                 src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-2.png"
-  //                 alt="Jacob Warnhalter"
-  //                 radius="xl"
-  //                 size="sm"
-  //               />
-  //               <span>Author</span>
-  //             </div>
-  //           </div>
-  //           <div className={styles.body}>
-  //             <Link className={styles.cardContent} href={Routes.Catalog()}>
-  //               <div className={styles.headerContainer}>
-  //                 <h2>Name</h2>
-  //                 <span>10</span>
-  //               </div>
-  //               <h3>Description</h3>
-  //             </Link>
-  //             <div className={styles.inline}>
-  //               <span className={styles.userImg}></span>
-  //               <span>Author</span>
-  //             </div>
-  //           </div>{" "}
-  //           <div className={styles.body}>
-  //             <div className={styles.cardContent}>
-  //               <div className={styles.headerContainer}>
-  //                 <h2>Name</h2>
-  //                 <span>10</span>
-  //               </div>
-  //               <h3>Description</h3>
-  //             </div>
-  //             <div className={styles.inline}>
-  //               <span className={styles.userImg}></span>
-  //               <span>Author</span>
-  //             </div>
-  //           </div>{" "}
-  //           <div className={styles.body}>
-  //             <div className={styles.cardContent}>
-  //               <div className={styles.headerContainer}>
-  //                 <h2>Name</h2>
-  //                 <span>10</span>
-  //               </div>
-  //               <h3>Description</h3>
-  //             </div>
-  //             <div className={styles.inline}>
-  //               <span className={styles.userImg}></span>
-  //               <span>Author</span>
-  //             </div>
-  //           </div>{" "}
-  //           <div className={styles.body}>
-  //             <div className={styles.cardContent}>
-  //               <div className={styles.headerContainer}>
-  //                 <h2>Name</h2>
-  //                 <span>10</span>
-  //               </div>
-  //               <h3>Description</h3>
-  //             </div>
-  //             <div className={styles.inline}>
-  //               <span className={styles.userImg}></span>
-  //               <span>Author</span>
-  //             </div>
-  //           </div>
-  //           <div className={styles.body}>
-  //             <div className={styles.cardContent}>
-  //               <div className={styles.headerContainer}>
-  //                 <h2>Name</h2>
-  //                 <span>10</span>
-  //               </div>
-  //               <h3>Description</h3>
-  //             </div>
-  //             <div className={styles.inline}>
-  //               <span className={styles.userImg}></span>
-  //               <span>Author</span>
-  //             </div>
-  //           </div>
-  //           <div className={styles.body}>
-  //             <div className={styles.cardContent}>
-  //               <div className={styles.headerContainer}>
-  //                 <h2>Name</h2>
-  //                 <span>10</span>
-  //               </div>
-  //               <h3>Description</h3>
-  //             </div>
-  //             <div className={styles.inline}>
-  //               <span className={styles.userImg}></span>
-  //               <span>Author</span>
-  //             </div>
-  //           </div>
-  //         </>
-  //       )
-  //     case "public":
-  //       return <>{publicCatalogs}</>
-  //     case "shared":
-  //       return <div>{sharedCatalogs}</div>
-  //     case "own":
-  //       return <div>{ownCatalogs}</div>
-  //   }
-  // }
 
   const handleFilter = (type: FilterType) => {
     dispatch({
@@ -487,20 +179,6 @@ const Catalogs: BlitzPage = ({ id, image, header, desc, isFavorite, authorId }: 
     })
   }
 
-  // const router = useRouter()
-
-  // const handlePick = (item: PickerOption) => {
-  //   setSelected(item)
-  //   void router
-  //     .push({
-  //       pathname: pathname,
-  //       query: { ...router.query, type: item.value },
-  //     })
-  //     .catch((error) => {
-  //       console.error("Failed to navigate", error)
-  //     })
-  // }
-
   return (
     <Layout title="Catalogs">
       <main className={styles.main}>
@@ -517,18 +195,31 @@ const Catalogs: BlitzPage = ({ id, image, header, desc, isFavorite, authorId }: 
               { label: "Own", value: "own" },
             ]}
           />
-          <TextInput value={searchValue} onChange={handleTextChange} />
-
-          <Flex align={"center"} justify={"space-between"}>
-            <label style={{ fontSize: "var(--mantine-font-size-sm)", fontWeight: 500 }}>
-              Sort by:{" "}
+          <Flex align="center" justify="space-between">
+            <label
+              style={{ fontSize: "var(--mantine-font-size-sm)", fontWeight: 500 }}
+              htmlFor="search"
+            >
+              Search:
             </label>
-            <Box w="270px">
-              <Picker options={sharedWith} onChange={handleSortChange} />
+            <Box w="240px">
+              <TextInput value={searchValue} onChange={handleTextChange} id="search" />
+            </Box>
+          </Flex>
+
+          <Flex align="center" justify="space-between">
+            <label
+              style={{ fontSize: "var(--mantine-font-size-sm)", fontWeight: 500 }}
+              htmlFor="sort"
+            >
+              Sort by:
+            </label>
+            <Box w="240px">
+              <Picker options={sortOrder} onChange={handleSortChange} id="sort" />
             </Box>
           </Flex>
         </div>
-        {/* <div className={styles.gridCatalogs}>{catalogContent(catalogType)}</div> */}
+
         <div className={styles.gridCatalogs}>
           {catalog.map((c) => (
             <Catalog
