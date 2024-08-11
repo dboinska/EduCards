e79c87807f9567e22b18786c68ac8d5326617e62
Dorota Boinska <dboinska@gmail.com>
2024-08-11 23:45:44 +0200

    feat(pages): add catalog creator

---
+++ src/pages/catalogs/new/add-cards.tsx
@@ -0,0 +1,205 @@
+import { Routes, type BlitzPage } from "@blitzjs/next"
+import { useRouter } from "next/router"
+import { Box, Button, Center, Flex, Group, Stepper, Textarea, TextInput } from "@mantine/core"
+import { randomId } from "@mantine/hooks"
+
+import Layout from "@/core/layouts/Layout"
+import styles from "src/styles/Catalogs.module.css"
+
+import { CreateCatalogLayout } from "@/layouts/CreateCatalogLayout"
+import { useCatalogContext } from "@/contexts/CreateCatalog.context"
+
+import type { CreateCatalogContextProps } from "@/contexts/CreateCatalog.context"
+import { useForm } from "@mantine/form"
+import { zodResolver } from "mantine-form-zod-resolver"
+import { NewCatalogCardsSchema, newCatalogCardsSchema } from "@/schemas/CreateCatalog.schema"
+
+import { cardDefaults } from "@/schemas/Card.defaults"
+import { IconCirclePlus, IconGripVertical, IconX } from "@tabler/icons-react"
+import { ImageUpload } from "@/components/ImageUpload"
+import { DragDropContext, Draggable, Droppable } from "@hello-pangea/dnd"
+import { createCatalogCardsDefaults } from "@/schemas/CreateCatalog.defaults"
+
+const { catalogId: _, ...cardInitial } = cardDefaults
+
+const NewCatalogAddCards: BlitzPage = () => {
+  const { formState, setFormState } = useCatalogContext() as CreateCatalogContextProps
+  const { push } = useRouter()
+
+  console.log("renderer")
+
+  const form = useForm<NewCatalogCardsSchema>({
+    validate: zodResolver(newCatalogCardsSchema),
+    initialValues: { ...createCatalogCardsDefaults, ...formState },
+    validateInputOnChange: true,
+    validateInputOnBlur: true,
+  })
+
+  console.log({ error: form.errors, values: form.values })
+
+  const handleSubmit = async (values: NewCatalogCardsSchema) => {
+    console.log({ values })
+
+    setFormState((state) => ({ ...state, ...values }))
+    await push(Routes.NewCatalogShareSettingsPage())
+  }
+
+  const handleBack = async () => {
+    await push(Routes.NewCatalogPage())
+  }
+
+  const handleOnDrop = async (files: any, index: number) => {
+    const formData = new FormData()
+    formData.append("file", files[0])
+
+    try {
+      const response = await fetch("/api/catalog/upload-cover", {
+        method: "POST",
+        body: formData,
+      })
+
+      if (!response.ok) {
+        throw new Error("Failed to upload cover")
+      }
+
+      const result = await response.json()
+
+      form.setFieldValue(`cards.${index}.imageURL`, result.fileURL)
+      console.log("File uploaded successfully", result)
+    } catch (error) {
+      console.error("Error uploading cover", error)
+    }
+  }
+
+  const cards = form.getValues().cards.map((item, index) => (
+    <Draggable key={item.key} index={index} draggableId={index.toString()}>
+      {(provided) => (
+        <Group
+          ref={provided.innerRef}
+          mt="xs"
+          {...provided.draggableProps}
+          className={styles.mantineDropzoneInner}
+          component="fieldset"
+          key={item.key}
+        >
+          <Flex justify={"space-between"} className={styles.fullWidth}>
+            <Center {...provided.dragHandleProps}>
+              <IconGripVertical size="1rem" />
+              Press and slide to change the order
+            </Center>
+            {cards.length > 1 && <IconX onClick={() => removeCard({ index })} />}
+          </Flex>
+          <legend>Card #{index + 1} </legend>
+          <Flex direction="column" className={styles.fullWidth}>
+            <Flex wrap="wrap" gap="8px" justify="space-between" className={styles.fullWidth}>
+              <Textarea
+                size="sm"
+                radius="md"
+                label="Term"
+                placeholder="Term"
+                withAsterisk
+                {...form.getInputProps(`cards.${index}.term`)}
+                className={styles.textarea}
+                error={form.errors?.[`cards.${index}.term`]}
+              />
+              <Textarea
+                size="sm"
+                radius="md"
+                label="Description"
+                placeholder="Description text"
+                {...form.getInputProps(`cards.${index}.description`)}
+                className={styles.textarea}
+              />
+            </Flex>
+          </Flex>
+          <ImageUpload
+            onDrop={(files) => handleOnDrop(files, index)}
+            onReject={(file) => console.log({ file })}
+          />
+          <Flex wrap={"wrap"} gap={"8px"} className={styles.fullWidth}>
+            <Textarea
+              size="sm"
+              radius="md"
+              label="Definition"
+              withAsterisk
+              placeholder="Definition"
+              {...form.getInputProps(`cards.${index}.termTranslated`)}
+              className={styles.textarea}
+              error={form.errors?.[`cards.${index}.termTranslated`]}
+            />
+            <Textarea
+              size="sm"
+              radius="md"
+              label="Description"
+              placeholder="Description"
+              {...form.getInputProps(`cards.${index}.descriptionTranslated`)}
+              className={styles.textarea}
+            />
+          </Flex>
+        </Group>
+      )}
+    </Draggable>
+  ))
+
+  const removeCard = (index) => {
+    console.log({ index })
+    form.removeListItem("cards", index.index)
+  }
+
+  return (
+    <Layout title="Add cards to catalog">
+      <main className={styles.main}>
+        <Box className={styles.container}>
+          <Stepper active={1}>
+            <Stepper.Step label="First step" description="Main settings" />
+            <Stepper.Step label="Second step" description="Adding cards to catalog" />
+            <Stepper.Step label="Final step" description="Catalog sharing" />
+          </Stepper>
+        </Box>
+
+        <form name="addCards" onSubmit={form.onSubmit(handleSubmit)} className={styles.container}>
+          <DragDropContext
+            onDragEnd={({ destination, source }) =>
+              destination?.index !== undefined &&
+              form.reorderListItem("cards", { from: source.index, to: destination.index })
+            }
+          >
+            <Droppable droppableId="dnd-list" direction="vertical">
+              {(provided) => (
+                <div {...provided.droppableProps} ref={provided.innerRef}>
+                  {cards}
+                  {provided.placeholder}
+                </div>
+              )}
+            </Droppable>
+          </DragDropContext>
+          <Group justify="center" mt="md">
+            <Button
+              variant="filled"
+              color="var(--mantine-color-blue-6)"
+              radius="md"
+              disabled={!form.isValid()}
+              onClick={() => form.insertListItem("cards", { ...cardInitial, key: randomId() })}
+            >
+              <IconCirclePlus /> Add card
+            </Button>
+          </Group>
+          <Group justify="flex-end" mt="xl">
+            <Button variant="default" onClick={handleBack}>
+              Back
+            </Button>
+            <Button type="submit" disabled={!form.isValid()}>
+              Next step
+            </Button>
+          </Group>
+        </form>
+      </main>
+    </Layout>
+  )
+}
+
+NewCatalogAddCards.getLayout = function getLayout(page) {
+  return <CreateCatalogLayout>{page}</CreateCatalogLayout>
+}
+
+export default NewCatalogAddCards
---
+++ src/pages/catalogs/new/index.tsx
@@ -0,0 +1,121 @@
+import { Routes, type BlitzPage } from "@blitzjs/next"
+import { useRouter } from "next/router"
+import { Box, Button, NativeSelect, Stepper, TextInput } from "@mantine/core"
+import { useForm } from "@mantine/form"
+import { zodResolver } from "mantine-form-zod-resolver"
+
+import Layout from "@/core/layouts/Layout"
+import styles from "src/styles/Catalogs.module.css"
+import { ImageUpload } from "@/components/ImageUpload"
+
+import { CreateCatalogLayout } from "@/layouts/CreateCatalogLayout"
+import { useCatalogContext } from "@/contexts/CreateCatalog.context"
+
+import {
+  type CreateCatalogBaseSchema,
+  createCatalogBaseSchema,
+} from "@/schemas/CreateCatalog.schema"
+import { createCatalogBaseDefaults } from "@/schemas/CreateCatalog.defaults"
+
+import type { CreateCatalogContextProps } from "@/contexts/CreateCatalog.context"
+
+const NewCatalog: BlitzPage = () => {
+  const { formState, setFormState } = useCatalogContext() as CreateCatalogContextProps
+  const { push } = useRouter()
+
+  const form = useForm({
+    mode: "uncontrolled",
+    validate: zodResolver(createCatalogBaseSchema),
+    initialValues: { createCatalogBaseDefaults, ...formState },
+    validateInputOnChange: true,
+    validateInputOnBlur: true,
+  })
+
+  const handleSubmit = async (values: CreateCatalogBaseSchema) => {
+    console.log({ values })
+
+    setFormState((state) => ({ ...state, ...values }))
+    await push(Routes.NewCatalogAddCards())
+  }
+
+  const handleOnDrop = async (files) => {
+    const formData = new FormData()
+    formData.append("file", files[0])
+
+    try {
+      const response = await fetch("/api/catalog/upload-cover", {
+        method: "POST",
+        body: formData,
+      })
+
+      if (!response.ok) {
+        throw new Error("Failed to upload cover")
+      }
+
+      const result = await response.json()
+
+      form.setFieldValue("imageUrl", result.fileURL)
+      console.log("File uploaded successfully", result)
+    } catch (error) {
+      console.error("Error uploading cover", error)
+    }
+  }
+
+  return (
+    <Layout title="Create new catalog">
+      <main className={styles.main}>
+        <Box className={styles.container}>
+          <Stepper active={0}>
+            <Stepper.Step label="First step" description="Main settings" />
+            <Stepper.Step label="Second step" description="Adding cards to catalog" />
+            <Stepper.Step label="Final step" description="Catalog sharing" />
+          </Stepper>
+        </Box>
+
+        <form name="newCatalog" onSubmit={form.onSubmit(handleSubmit)} className={styles.container}>
+          <TextInput
+            label="Catalog name"
+            withAsterisk
+            placeholder="Catalog name"
+            error={form.errors.catalogueName}
+            {...form.getInputProps("name", { isRequired: true })}
+          />
+          <TextInput
+            mt="sm"
+            label="Description"
+            placeholder="Description"
+            {...form.getInputProps("description")}
+          />
+          <ImageUpload onDrop={handleOnDrop} onReject={(file) => console.log({ file })} />
+          <NativeSelect
+            label="Number of drawers"
+            component="select"
+            data={["3", "5", "7"]}
+            mt="md"
+            {...form.getInputProps("amountOfDrawers")}
+          />
+          <Box pt="md" ta="right">
+            <Button type="submit" disabled={!form.isValid()}>
+              Next step
+            </Button>
+          </Box>
+        </form>
+      </main>
+    </Layout>
+  )
+}
+
+NewCatalog.getLayout = function getLayout(page) {
+  return <CreateCatalogLayout>{page}</CreateCatalogLayout>
+}
+
+export default NewCatalog
+
+/*
+  @TODO: Ograniczenia uploadu tylko do plików max 5 MB i plików graicznych
+  @TODO: Dostęp do widoku tylko dla zalogowanych userów
+  @TODO: Upload covera bezpośrednio do katalogu usera
+
+  ## Szufladki
+  Number of drawers - podczas zapisu do bazy danych pole będzie wskazywać ile rekordów w tabeli drawers powinno zostać utworzonych dla poszczególnego katalogu. Pole wirtualne, obowiązkowe, default 3.
+ */
