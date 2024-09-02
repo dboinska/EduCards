c6d2beab43163394e1f6fb9e6217697a60bbe2df
Dorota Boinska <dboinska@gmail.com>
2024-09-03 01:28:02 +0200

    refactor(pages): improve CatalogId page

--- src/pages/catalogs/[id]/index.tsx
+++ src/pages/catalogs/[id]/index.tsx
@@ -14,14 +14,12 @@ import getDrawer from "../queries/getDrawer"
 import { useQuery } from "@blitzjs/rpc"
 import { UseDrawer } from "@/core/providers/drawerProvider"
 import { useSession } from "@blitzjs/auth"
-import { useState, useReducer, useEffect } from "react"
+import { useState, useReducer } from "react"
 import { useDebouncedCallback } from "@mantine/hooks"
 import { actionTypes, dataReducer, initialState } from "@/reducers/dataReducer"
 import { useRouter } from "next/router"
 import { SortType } from "@/types/SortType"
 import { CatalogCard } from "@/components/CatalogCard"
-import getUsers from "../../../users/queries/getUsers"
-import { UserSchema } from "@/schemas/User.schema"
 
 const CatalogId: BlitzPage<InferGetServerSidePropsType<typeof getServerSideProps>> = ({
   query,
@@ -30,31 +28,21 @@ const CatalogId: BlitzPage<InferGetServerSidePropsType<typeof getServerSideProps
   const router = useRouter()
   const [catalogId] = useQuery(getDrawer, {})
   const { drawerProps, setDrawerProps } = UseDrawer()
-  const [catalogState, dispatch] = useReducer(dataReducer, {
+  const [_, dispatch] = useReducer(dataReducer, {
     ...initialState,
     ...query,
   })
   const [searchValue, setSearchValue] = useState(() => query.query || "")
+  const [cards, setCards] = useState(() => catalog?.cards || [])
+
+  const handleCardDelete = (cardId: string) => {
+    setCards((prevCards) => prevCards.filter((card) => card.cardId !== cardId))
+  }
 
   const drawers = catalogId.drawers
   const session = useSession({ suspense: false })
 
   const ownerId = session?.userId || null
-
-  const isFavorite = false
-
-  const cardSettings = [
-    {
-      label: "Edit",
-      path: Routes.NewCatalog(),
-      id: "edit",
-    },
-    {
-      label: "Delete",
-      path: Routes.Catalogs(),
-      id: "delete",
-    },
-  ]
 
   const handleSortChange = async ({ value }: PickerOption) => {
     dispatch({
@@ -85,6 +73,22 @@ const CatalogId: BlitzPage<InferGetServerSidePropsType<typeof getServerSideProps
     await router.push(routerQuery)
   }, 500)
 
+  const catalogItems = cards.map((c) => (
+    <CatalogCard
+      key={c.cardId}
+      // owner={c.ownerId}
+      imageUrl={c.imageURL}
+      term={c.term}
+      description={c.description}
+      // settings={cardSettings}
+      isOwner={c.ownerId === ownerId}
+      owner={catalog?.owner}
+      cardId={c.cardId}
+      catalogId={c.catalogId}
+      onDelete={handleCardDelete}
+    />
+  ))
+
   return (
     <Layout title={`Catalog ${catalog?.name}`}>
       <main className={styles.main}>
@@ -105,7 +109,6 @@ const CatalogId: BlitzPage<InferGetServerSidePropsType<typeof getServerSideProps
             />
           </div>
         </Box>
-
         <h2>All cards:</h2>
         <div className={styles.filters}>
           <Flex align="center" justify="space-between">
@@ -119,7 +122,6 @@ const CatalogId: BlitzPage<InferGetServerSidePropsType<typeof getServerSideProps
               <TextInput value={searchValue} onChange={handleTextChange} id="search" />
             </Box>
           </Flex>
-
           <Flex align={"center"} justify={"space-between"}>
             <label
               style={{
@@ -134,22 +136,7 @@ const CatalogId: BlitzPage<InferGetServerSidePropsType<typeof getServerSideProps
             </Box>
           </Flex>
         </div>
-        <div className={styles.gridCatalogs}>
-          {catalog?.cards.map((c) => {
-            return (
-              <CatalogCard
-                key={c.cardId}
-                ownerId={c.ownerId}
-                imageUrl={c.imageURL}
-                term={c.term}
-                description={c.description}
-                settings={cardSettings}
-                isOwner={c.ownerId === ownerId}
-                owner={catalog.owner}
-              />
-            )
-          })}
-        </div>
+        <div className={styles.gridCatalogs}>{catalogItems}</div>
       </main>
     </Layout>
   )
