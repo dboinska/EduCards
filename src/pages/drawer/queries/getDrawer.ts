e4ec2a2fb2a88d6d22ca66b3ef48d7fcdfbf561f
Dorota Boinska <dboinska@gmail.com>
2024-10-17 01:27:37 +0200

    refactor(drawer): remove unused code and comments in drawer and queries

--- src/pages/drawer/queries/getDrawer.ts
+++ src/pages/drawer/queries/getDrawer.ts
@@ -2,22 +2,16 @@ import type { Ctx } from "blitz"
 import db from "db"
 import { catalogSchema, type CatalogSchema } from "@/schemas/Catalog.schema"
 
-export default async function getCatalog(input: CatalogSchema, ctx: Ctx) {
+export default async function getDrawer(input: CatalogSchema, ctx: Ctx) {
   const data = catalogSchema.parse(input)
   console.log({ input })
 
   const catalog = await db.drawer.findUnique({
     include: {
       catalog: true,
-      cards: true,
     },
     where: {
       drawerId: data.id,
-      // cards: {
-      //   is: {
-      //     name: "bob",
-      //   },
-      // },
     },
   })
 
@@ -25,9 +19,7 @@ export default async function getCatalog(input: CatalogSchema, ctx: Ctx) {
     return null
   }
 
-  // const { id, email, imageUrl, name } = catalog?.owner
   console.log({ catalog })
 
-  // return { ...catalog, owner: { id, email, imageUrl, name } }
   return catalog
 }
