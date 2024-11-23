aa9ce9c94100ca936efcae14f8ecf212dffa7b02
Dorota Boinska <dboinska@gmail.com>
2024-11-23 23:40:17 +0100

    feat(pages): add edit, improve index and new study plan page

--- src/pages/studyPlan/[id]/edit.tsx
+++ src/pages/studyPlan/[id]/edit.tsx
@@ -1,124 +1,142 @@
-import Layout from "src/core/layouts/Layout"
 import { BlitzPage, Routes } from "@blitzjs/next"
+import Layout from "src/core/layouts/Layout"
 import styles from "src/styles/Quiz.module.css"
 
 import { SliderInput } from "@/components/SliderInput"
-import {
-  Button,
-  Flex,
-  Input,
-  MantineProvider,
-  NativeSelect,
-  TextInput,
-  createTheme,
-  Text,
-} from "@mantine/core"
+import { Button, Flex, TextInput, MantineProvider, NativeSelect, createTheme } from "@mantine/core"
 import { IconCheck } from "@tabler/icons-react"
 import { ColorSelection } from "@/components/ColorSelector"
 import { DateInput } from "@mantine/dates"
 import { useMutation } from "@blitzjs/rpc"
-import createStudyPlan from "./mutations/createStudyPlan"
 import { useRouter } from "next/router"
-import { useForm } from "@mantine/form"
-import { zodResolver } from "mantine-form-zod-resolver"
-import { createStudyPlanSchema, CreateStudyPlanSchema } from "@/schemas/CreateStudyPlan.schema"
+import { useForm, zodResolver } from "@mantine/form"
 import { gSSP } from "@/blitz-server"
-import getCatalogs from "../catalogs/queries/getCatalogs"
 import { InferGetServerSidePropsType } from "next"
 import { useEffect } from "react"
-import getStudyPlans from "./queries/getStudyPlans"
+import getCatalogs from "@/pages/catalogs/queries/getCatalogs"
+import getStudyPlans from "../queries/getStudyPlans"
+import updateStudyPlan from "../mutations/updateStudyPlan"
+import { UpdateStudyPlanSchema, updateStudyPlanSchema } from "@/schemas/UpdateStudyPlan.schema"
+import getStudyPlan from "../queries/getStudyPlan"
+import { z } from "zod"
 
 const theme = createTheme({
   components: {
-    Input: Input.extend({
+    Input: {
       defaultProps: {
         variant: "filled",
       },
-    }),
-
-    InputWrapper: Input.Wrapper.extend({
+    },
+    InputWrapper: {
       defaultProps: {
         inputWrapperOrder: ["label", "input", "description", "error"],
       },
-    }),
+    },
   },
 })
 
-const NewStudyPlan: BlitzPage<InferGetServerSidePropsType<typeof getServerSideProps>> = ({
+const updateStudyPlanSchemaWithoutId = updateStudyPlanSchema.omit({ id: true })
+type UpdateStudyPlanSchemaWithoutId = z.infer<typeof updateStudyPlanSchemaWithoutId>
+
+console.log({ updateStudyPlanSchemaWithoutId })
+
+const EditStudyPlan: BlitzPage<InferGetServerSidePropsType<typeof getServerSideProps>> = ({
   userCatalogs,
+  catalogs,
+  query,
+  studyPlan,
   userId,
 }) => {
-  const [createStudyPlanMutation] = useMutation(createStudyPlan)
-  console.log({ userCatalogs })
+  const [updateStudyPlanMutation] = useMutation(updateStudyPlan)
+  const router = useRouter()
+
+  console.log({ query })
 
   const formattedCatalogs = userCatalogs.map((catalog) => ({
     value: catalog.catalogId,
     label: catalog.name,
   }))
 
+  const { studyPlan: sp } = studyPlan || {}
+
   const initialValues = {
-    name: "Study Plan",
-    daysPerWeek: 4,
-    minutesPerDay: 15,
-    wordsPerDay: 4,
-    completionDate: new Date(new Date().setDate(new Date().getDate() + 30)),
-    color: "black",
-    catalogId: formattedCatalogs.length > 0 ? formattedCatalogs[0]?.value : "",
+    name: sp?.name,
+    daysPerWeek: sp?.daysPerWeek,
+    minutesPerDay: sp?.secondsPerDay ? Math.ceil(sp.secondsPerDay / 60) : 30,
+    wordsPerDay: sp?.wordsPerDay,
+    completionDate: sp?.completionDate
+      ? new Date(sp.completionDate)
+      : new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
+    color: sp?.color,
+    catalogId: sp?.catalogId,
   }
 
-  const router = useRouter()
+  const catalogId = initialValues.catalogId
 
-  const form = useForm({
-    validate: zodResolver(createStudyPlanSchema),
+  const currentCatalog = {
+    value: catalogId,
+    label: catalogs?.find((catalog) => catalog.catalogId === catalogId)?.name || "",
+  }
+
+  console.log({ currentCatalog })
+
+  const catalogsWithPrevious = formattedCatalogs.some((c) => c.value === catalogId)
+    ? formattedCatalogs
+    : [...formattedCatalogs, currentCatalog]
+
+  const form = useForm<UpdateStudyPlanSchemaWithoutId>({
     initialValues,
+    validate: zodResolver(updateStudyPlanSchemaWithoutId),
     validateInputOnChange: true,
+    validateInputOnBlur: true,
   })
+
   useEffect(() => {
-    console.log({ values: form.values })
+    console.log("Current form values:", form.values)
     console.log("Current form errors:", form.errors)
+    console.log({ isValid: form.isValid() })
   }, [form])
 
-  const handleSubmit = async (values: CreateStudyPlanSchema) => {
-    console.log({ submited: values })
+  const handleSubmit = async (values: Omit<UpdateStudyPlanSchema, "id">) => {
     try {
-      await createStudyPlanMutation(values)
+      await updateStudyPlanMutation({
+        ...values,
+        // completionDate: values.completionDate.toString(),
+        id: query.id as string,
+      })
       await router.push(Routes.StudyPlans())
     } catch (error: any) {
-      console.log(error)
+      console.error("Error creating study plan:", error)
     }
   }
 
   return (
-    <Layout title="New study plan">
+    <Layout title="Edit Study Plan">
       <main className={styles.main}>
         <div className={styles.header}>
-          <h1>New Study Plan</h1>
+          <h1>Edit Study Plan</h1>
         </div>
         <Flex w="100%" justify="center">
           <Flex direction="column" w="90%">
             <MantineProvider theme={theme}>
-              <form name="newStudyPlan" onSubmit={form.onSubmit(handleSubmit)}>
+              <form onSubmit={form.onSubmit(handleSubmit)}>
                 <TextInput
                   label="Study plan name"
                   m="sm"
                   placeholder="Study plan name"
                   {...form.getInputProps("name")}
                 />
-                {formattedCatalogs.length > 0 ? (
-                  <NativeSelect
-                    label="Select catalog"
-                    m="sm"
-                    data={formattedCatalogs}
-                    {...form.getInputProps("catalogId")}
-                  />
-                ) : (
-                  <Text m="sm">No catalogs available.</Text>
-                )}
+                <NativeSelect
+                  label="Select catalog"
+                  m="sm"
+                  data={catalogsWithPrevious}
+                  placeholder="Select catalog"
+                  {...form.getInputProps("catalogId")}
+                />
                 <ColorSelection
                   value={form.values.color}
                   onChange={(color) => form.setFieldValue("color", color)}
                 />
-
                 <SliderInput
                   label="Number of study days per week"
                   step={1}
@@ -143,17 +161,16 @@ const NewStudyPlan: BlitzPage<InferGetServerSidePropsType<typeof getServerSidePr
                 <DateInput
                   m="sm"
                   clearable
-                  defaultValue={new Date()}
                   label="Planned date of completion"
                   placeholder="Planned date of completion"
+                  value={new Date(form.values.completionDate)}
                   {...form.getInputProps("completionDate")}
                 />
-
                 <Flex justify="right" m="var(--mantine-spacing-md) 0">
                   <Button
                     type="submit"
                     variant="filled"
-                    color="var(--mantine-color-lime-6)"
+                    color="lime"
                     radius="md"
                     disabled={!form.isValid()}
                   >
@@ -170,19 +187,19 @@ const NewStudyPlan: BlitzPage<InferGetServerSidePropsType<typeof getServerSidePr
 }
 
 export const getServerSideProps = gSSP(async ({ query, ctx }) => {
+  const studyPlan = await getStudyPlan(query, ctx)
   const catalogs = await getCatalogs(query, ctx)
   const studyPlans = await getStudyPlans(query, ctx)
-
   const userId = ctx.session.userId
 
   const usedCatalogIds = new Set(studyPlans.map((plan) => plan.catalogId))
-
   const userCatalogs = catalogs.filter(
     (catalog) => catalog?.ownerId === userId && !usedCatalogIds.has(catalog.catalogId)
   )
+
   return {
-    props: { query, userCatalogs, userId },
+    props: { query, studyPlan, catalogs, userCatalogs, userId },
   }
 })
 
-export default NewStudyPlan
+export default EditStudyPlan
