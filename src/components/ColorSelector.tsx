d71eb02ea1281ea251a9b36866c50cbdc4fa7c49
Dorota Boinska <dboinska@gmail.com>
2024-11-23 23:53:33 +0100

    feat(components): improve ColorSelector

--- src/components/ColorSelector.tsx
+++ src/components/ColorSelector.tsx
@@ -1,31 +1,45 @@
 import { Flex, ColorSwatch, SimpleGrid, useMantineTheme } from "@mantine/core"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
-export function ColorSelection() {
+interface ColorSelectionProps {
+  value: string
+  onChange: (color: string) => void
+}
+
+export function ColorSelection({ value, onChange }: ColorSelectionProps) {
   const theme = useMantineTheme()
   const colors = Object.keys(theme.colors).map((color) => theme.colors[color][6])
 
-  const [selectedColor, setSelectedColor] = useState(colors[0])
+  const [checked, setChecked] = useState(value)
 
-  const CustomColorSwatch = ({ color }) => (
-    <ColorSwatch
-      color={color}
-      style={{
-        cursor: "pointer",
-        border: color === selectedColor ? "2px solid #000" : "1px solid rgba(0, 0, 0, 0.1)",
-        transform: color === selectedColor ? "scale(1.1)" : "scale(1)",
-        transition: "transform 0.1s, border 0.1s",
-      }}
-      onClick={() => setSelectedColor(color)}
-    />
-  )
+  useEffect(() => {
+    console.log("Current selected color (value):", value)
+  }, [value])
+
+  const handleColorClick = (color: string) => {
+    setChecked(color)
+    onChange(color)
+  }
 
   return (
     <Flex w="100%" direction="column" m="var(--mantine-spacing-md)">
       <label style={{ display: "block", marginBottom: 10 }}>Select main color:</label>
       <SimpleGrid cols={15} spacing="sm" style={{ display: "flex", flexWrap: "wrap" }}>
         {colors.map((color) => (
-          <CustomColorSwatch key={color} color={color} />
+          <ColorSwatch
+            key={color}
+            color={color}
+            onClick={() => handleColorClick(color)}
+            style={{
+              cursor: "pointer",
+              border:
+                color === checked
+                  ? "2px solid var(--mantine-color-blue-6)"
+                  : "1px solid rgba(0, 0, 0, 0.1)",
+              transform: color === checked ? "scale(1.1)" : "scale(1)",
+              transition: "transform 0.2s, border 0.2s",
+            }}
+          />
         ))}
       </SimpleGrid>
     </Flex>
